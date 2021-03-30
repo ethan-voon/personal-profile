2174c37d946000cd8a36cdc9ca7cbcdfaef4dc55
Ethan Voon <epvoon@gmail.com>
2021-03-30 11:53:41 -0700

    Add tooltips. Reorganize blog and sign-up form.

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { PageProps } from '../App';
 import CardContent from '@material-ui/core/CardContent';
-import { Typography } from '@material-ui/core';
+import { CardActions, Grid, Typography } from '@material-ui/core';
 import PageCard from './page-card';
 import axios from 'axios';
+import $ from 'jquery';
 
 const useStyles = makeStyles((theme) => ({
+	cardActions: {
+		justifyContent: 'center',
+	},
 	postPreview: {
 		justifyContent: 'left',
 		width: '100%',
@@ -16,27 +20,29 @@ const useStyles = makeStyles((theme) => ({
 // https://developer.wordpress.org/rest-api/reference/posts/
 interface Post {
 	id: number;
-	date?: string;
+	date: string;
 	title: string;
 	content: string;
 	excerpt: string;
 }
 
-interface PreviewProps {
+interface PostProps {
 	post: Post;
 }
 
-function PostPreview(props: PreviewProps) {
+function PostPreview(props: PostProps) {
 	const classes = useStyles();
+	const date = new Date(props.post.date).toDateString();
+	// @TODO each excerpt should link to the full post, passing the post via props
 	if (props.post) {
 		return (
 			<PageCard>
 				<CardContent classes={{ root: classes.postPreview }}>
 					<Typography variant="h5">{props.post.title}</Typography>
-					<Typography variant="caption">{props.post.date}</Typography>
+					<Typography variant="caption">{date}</Typography>
 					<Typography variant="body1">
 						{
-							new DOMParser().parseFromString(props.post.content, 'text/html')
+							new DOMParser().parseFromString(props.post.excerpt, 'text/html')
 								.documentElement.textContent
 						}
 					</Typography>
@@ -49,6 +55,7 @@ function PostPreview(props: PreviewProps) {
 }
 
 export default function Blog(props: PageProps) {
+	const classes = useStyles();
 	const [posts, setPosts] = useState<Post[]>();
 	useEffect(() => {
 		axios
@@ -57,6 +64,23 @@ export default function Blog(props: PageProps) {
 				setPosts(res.data.posts);
 			})
 			.catch((error) => console.log(error));
+	}, []);
+
+	// For ConvertKit integration
+	useEffect(() => {
+		const script = document.createElement('script');
+
+		script.src = 'https://astounding-innovator-3833.ck.page/6b652f3c27/index.js';
+		script.async = true;
+
+		// Less than ethical hack to get rid of ConvertKit branding...
+		script.onload = () => $('div.formkit-powered-by-convertkit-container').remove();
+
+		document.body.appendChild(script);
+
+		return () => {
+			document.body.removeChild(script);
+		};
 	}, []);
 
 	const postPreviews: JSX.Element[] = [];
@@ -69,8 +93,43 @@ export default function Blog(props: PageProps) {
 	return (
 		<>
 			<PageCard>
-				<CardContent>{postPreviews}</CardContent>
+				<CardContent classes={{ root: classes.postPreview }}>
+					<Typography variant="h5">About the Blog</Typography>
+					<Typography variant="body1">
+						I constantly prevent myself from posting content online because I don't have
+						a single subject I want to post about. I like to think it's because I'm an
+						intensely curious generalist. I love to learn. It took awhile to break from
+						the negative impacts public education had on my learning mentality, but now
+						that I'm free from those shackles I have a newfound interest in everything I
+						come across. The problem is I have the attention span of a dog at a squirrel
+						park. I hop around knowledge domains so often that I end up not doing
+						anything with all this new data in my brain. My mission is to find community
+						and conversation by sharing my thoughts about what I learn. Come along if
+						you want!
+					</Typography>
+					<Grid
+						container
+						alignItems="center"
+						alignContent="center"
+						justify="center"
+						style={{ textAlign: 'center' }}
+					>
+						<Grid xs={12} component="span" item>
+							<CardContent>
+								<Typography variant={'h4'}>
+									Want to know when I write stuff?
+								</Typography>
+							</CardContent>
+						</Grid>
+						<Grid xs={12} component="span" item>
+							<CardActions classes={{ root: classes.cardActions }}>
+								<script async data-uid="6b652f3c27" />
+							</CardActions>
+						</Grid>
+					</Grid>
+				</CardContent>
 			</PageCard>
+			{postPreviews}
 		</>
 	);
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,16 +1,12 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, CardContent, CardActions } from '@material-ui/core';
+import { Grid, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import $ from 'jquery';
 import { PageProps, theme } from '../App';
 import PageCard from './page-card';
 
 const useStyles = makeStyles((theme) => ({
-	cardActions: {
-		justifyContent: 'center',
-	},
 	text: {
 		opacity: '1',
 		background: 'transparent',
@@ -20,70 +16,35 @@ const useStyles = makeStyles((theme) => ({
 export default function Home(props: PageProps) {
 	const classes = useStyles(theme);
 
-	// For ConvertKit integration
-	useEffect(() => {
-		const script = document.createElement('script');
-
-		script.src = 'https://astounding-innovator-3833.ck.page/6b652f3c27/index.js';
-		script.async = true;
-
-		// Less than ethical hack to get rid of ConvertKit branding...
-		script.onload = () => $('div.formkit-powered-by-convertkit-container').remove();
-
-		document.body.appendChild(script);
-
-		return () => {
-			document.body.removeChild(script);
-		};
-	}, []);
-
 	return (
 		<>
 			<Grid container component="span" alignItems="center" justify="center">
 				<Grid xs={12} component="span" item>
 					<Link to="/about" style={{ textDecoration: 'none' }}>
 						<PageCard>
-							<Typography variant={'h4'}>About</Typography>
-						</PageCard>
-					</Link>
-				</Grid>
-				<Grid xs={12} component="span" item>
-					<Link to="/projects" style={{ textDecoration: 'none' }}>
-						<PageCard>
-							<Typography variant={'h4'}>Projects</Typography>
+							<Tooltip title="Learn more about me.">
+								<Typography variant={'h4'}>About</Typography>
+							</Tooltip>
 						</PageCard>
 					</Link>
 				</Grid>
 				<Grid xs={12} component="span" item>
 					<Link to="/blog" style={{ textDecoration: 'none' }}>
 						<PageCard>
-							<Typography variant={'h4'}>Blog</Typography>
+							<Tooltip title="Sometimes I write stuff, click here to read on!">
+								<Typography variant={'h4'}>Blog</Typography>
+							</Tooltip>
 						</PageCard>
 					</Link>
 				</Grid>
 				<Grid xs={12} component="span" item>
-					<PageCard>
-						<Grid
-							container
-							alignItems="center"
-							alignContent="center"
-							justify="center"
-							style={{ textAlign: 'center' }}
-						>
-							<Grid xs={12} component="span" item>
-								<CardContent>
-									<Typography variant={'h4'}>
-										Want to know when I create content?
-									</Typography>
-								</CardContent>
-							</Grid>
-							<Grid xs={12} component="span" item>
-								<CardActions classes={{ root: classes.cardActions }}>
-									<script async data-uid="6b652f3c27" />
-								</CardActions>
-							</Grid>
-						</Grid>
-					</PageCard>
+					<Link to="/projects" style={{ textDecoration: 'none' }}>
+						<PageCard>
+							<Tooltip title="Side projects I'm working on.">
+								<Typography variant={'h4'}>Projects</Typography>
+							</Tooltip>
+						</PageCard>
+					</Link>
 				</Grid>
 			</Grid>
 		</>
